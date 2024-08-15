fc1cb1845ffff500f69373295f6e37b616cc7320
umuhozalionel <muhozalionel@gmail.com>
2024-08-15 19:53:07 +0000

    Subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     
-    if response.status_code != 200:
+    if response.status_code == 404:
         return 0
     
     try:
