dd7a5d63bff8ba30f583f0dd5999ddf3cf2d5e08
umuhozalionel <muhozalionel@gmail.com>
2024-08-15 19:47:00 +0000

    Subs numbers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"subscribers count"
+
 import requests
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    data = response.json()
+    
     if response.status_code != 200:
         return 0
-    else:
+    
+    try:
+        data = response.json()
         return data['data']['subscribers']
+    except ValueError:
+        print("Response content is not valid JSON")
+        return 0
+
